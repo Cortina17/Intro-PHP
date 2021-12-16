ea1171f8ed499d7ca157b431aa0a5cf9f21d0415
Cortina17 <diego.cortinagarcia@gmail.com>
2021-12-16 21:17:25 +0100

    exs 3 and 4 half-done

--- index.php
+++ index.php
@@ -10,26 +10,35 @@
 
 <body>
     <main>
-            <?php $frutas = ["manzana", "naranja", "limón", "pera"];?>
-            <h1>Lista de frutas</h1>
+            <h1>1. Lista de frutas</h1>
+            <?php $frutas = ["Limón", "Pera", "Manzana", "Plátano", "Cereza", "Fresa"];?>
                 <?php echo "
                 <li> $frutas[0]</li>
                 <li> $frutas[1]</li>
                 <li> $frutas[2]</li>
-                <li> $frutas[3]</li>"
+                <li> $frutas[3]</li>
+                <li> $frutas[4]</li>
+                <li> $frutas[5]</li>"
             ?>
             
-            <?php echo"
-                <input type='text'>
+            <h1>2. Input de texto, de radio y de selector</h1>
 
+            <?php echo"
+                <input type='text'>"
+            ?>
+
+            <?php echo"
                 <input type='radio' name = 'contact'>
                     <label>Tfno</label>
                 <input type='radio' name = 'contact'>
-                    <label>Fax</label>
+                    <label> Fax</label>
                 <input type='radio' name ='contact'>
-                    <label>Email</label>
+                    <label> Email</label>"
+            ?>
 
-                <select name = 'cars'>
+            <?php echo"
+                <select name = 'Car brands'>
+                    <option value='Select brand'>Select brand</option>
                     <option value='BMW'>BMW</option>
                     <option value='Volvo'>Volvo</option>
                     <option value='Seat'>Seat</option>
@@ -37,34 +46,122 @@
                 </select>"
             ?>
 
+            <h1>3. Array clave-valor de PHP en forma de tabla HTML</h1>
             <?php $user = 
                 [
                     [
                     'Name' => 'John',
                     'Surname' => 'Cena',
                     'Mail' => 'youcantseeme@hotmail.com',
+                    ],
+                    [
+                    'Name' => 'Rey',
+                    'Surname' => 'Misterio',
+                    'Mail' => 'booyakabooyaka@msn.com',
                     ]
                 ];
             ?>
 
-            <table border="3">'
+            <table border="3">
             
-            <?php
-                foreach($user as $r){
-            ?>
+            <?php foreach($user as $r){?>
                 <tr>
-                    <?php
-                    foreach($r as $v){
-                    ?>
-                    <td><?php echo $v;?></td>
-                    <?php
-                    }
-                    ?>
+                    <?php foreach($r as $v){?>
+                    <td>
+                    <?php echo $v;?>
+                    </td>
+                    <?php }?>
                 </tr>
                 <?php
             }
                 ?>
                 </table>
+
+            <h1>4.Cards de objetos película</h1>
+            <!-- Usa una clase para definir una película. Crea varios objetos Película y dibujarlos en “cards” en un documento HTML. -->
+            <?php 
+                Class Movie{
+                    private $title;
+                    public $year;
+                    public $director;
+
+                    function set_title($title)
+                    {
+                        $this->title = $title;
+                    }
+                    function get_title()
+                    {
+                        return $this->title;
+                    }
+
+                    function set_year($year)
+                    {
+                        $this->year = $year;
+                    }
+                    function get_year()
+                    {
+                        return $this->year;
+                    }
+
+                    function set_director($director)
+                    {
+                        $this->director = $director;
+                    }
+                    function get_director()
+                    {
+                        return $this->director;
+                    }
+                }
+            
+                $hulk = new Movie();
+                $hulk->set_title('The incredible Hulk');
+                $hulk->set_year('2008');
+                $hulk->set_director('Louis Leterrier');
+
+                echo "Title: " . $hulk->get_title();
+                    echo "<br>";
+                echo "Year: " . $hulk->get_year();
+                    echo "<br>";
+                echo "Director: " . $hulk->get_director();
+                    echo "<br>";
+
+                $iron = new Movie();
+                $iron->set_title('Iron Man');
+                $iron->set_year('2008');
+                $iron->set_director('Jon Favreau');
+
+                echo "Title: " . $iron->get_title();
+                    echo "<br>";
+                echo "Year: " . $iron->get_year();
+                    echo "<br>";
+                echo "Director: " . $iron->get_director();
+                    echo "<br>";
+
+                $thor = new Movie();
+                $thor->set_title('Thor');
+                $thor->set_year('2011');
+                $thor->set_director('Kenneth Branagh');
+
+                echo "Title: " . $thor->get_title();
+                    echo "<br>";
+                echo "Year: " . $thor->get_year();
+                    echo "<br>";
+                echo "Director: " . $thor->get_director();
+                    echo "<br>";
+                ?>
     </main>
 </body>
 </html>
+
+
+<!-- <li><ol><?php 
+            $frutas = ["Limón", "Pera", "Manzana", "Plátano", "Cereza", "Fresa"];
+            foreach($frutas as $key => $value){
+                echo "{$key} => {$value}";
+            };
+            foreach($frutas as $fruta){
+                $fruta++;
+                echo $fruta;
+            }
+        ?></ol></li> -->
+
